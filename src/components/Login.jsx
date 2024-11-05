75fd6316cfe2a55c7e271d387a9310b52674c4e0
webhostdevs <webhostdev59@gmail.com>
2024-11-05 16:19:56 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { AuthContext } from '../AuthContext';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const { loginWithRedirect, isAuthenticated } = useContext(AuthContext);
